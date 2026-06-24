7df57914f54c66c15c7fecf01b5c99c6c80fda16
Lai Vo <laivo2000@gmail.com>
2026-06-24 22:41:22 +0100

    update server for deploying  on render

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,17 @@ const analyzeRoute = require("./routes/analyze");
 
 const app = express();
 
+const PORT = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.json({ status: "ok" });
+});
+
 app.use("/api/analyze", analyzeRoute);
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
